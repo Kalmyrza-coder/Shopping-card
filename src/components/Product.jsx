33e34a85174790c644c2e273f10d4a54c970a3bf
Kalmyrza <Kalmyrzastudy@gmail.com>
2022-03-13 19:52:36 +0600

    created card in product with material ui

---
+++ src/components/Product.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import Card from '@mui/material/Card'
+import { CardActions } from '@mui/material'
+import { CardContent } from '@mui/material'
+import { CardMedia } from '@mui/material'
+import { Button } from '@mui/material'
+import { Typography } from '@mui/material'
+import { Grid } from '@mui/material'
+import { TextField } from '@mui/material'
+
+function Product({data}) {
+    const {image,title,description, price} = data
+    const [quantity, setQuantity] = useState(10)
+    return(
+        <><Card sx={{
+            maxWidth: 345,
+            mb: 10
+        }}>
+            <CardMedia 
+            component="img"
+            image={image}
+            alt="green iguana"
+            />
+            <Typography gutterBottom variant="h5" component='div'>
+                {title}
+            </Typography>
+            <Typography variant='body2' color="text.secondary">
+                {description}
+            </Typography>
+            <Typography m={2} variant='body1' color="text">
+                {price}
+            </Typography>
+            <Grid container spacing={2}>
+                <Grid item xs={3}><Button variant="contained" onClick={() => setQuantity(prev => prev + 1)}>Add one</Button></Grid>
+                <Grid item xs={3}><TextField disabled value={quantity}></TextField></Grid>
+                <Grid item xs={3}><Button variant="contained" onClick={() => setQuantity(prev => prev - 1)}>Remove one</Button></Grid>
+            </Grid>
+        </Card>
+        </>
+    )
+}
+
+export default Product;
